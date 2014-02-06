c0b6168235b90ce5a85d28ef7cf434b30f59a587
Sandy Huang <sandyhhuang@gmail.com>
2014-02-05 23:53:01 -0800

    Added code to auto_performance that sums up total time to select actions and total time to execute them

--- auto_performance.py
+++ auto_performance.py
@@ -11,7 +11,10 @@ def estimate_performance(results_file):
     num_knots = 0
     num_misgrasps = 0
     num_infeasible = 0
+    action_time = 0
+    exec_time = 0
     misgrasps_logged = True
+    timing_logged = True
 
     #for i_task in range(len(results_file)):
     for i_task in results_file:
@@ -40,6 +43,12 @@ def estimate_performance(results_file):
                 knot_exists = True
                 break
 
+            try:
+                action_time += step_info['action_time']
+                exec_time += step_info['exec_time']
+            except:
+                timing_logged = False
+
         if not knot_exists:
             if infeasible:
                 num_infeasible += 1
@@ -50,6 +59,9 @@ def estimate_performance(results_file):
     if misgrasps_logged:
         print "# Misgrasps:", num_misgrasps
         print "# Infeasible:", num_infeasible
+    if timing_logged:
+        print "Time taken to choose demo:", action_time
+        print "Time taken to warp and execute demo:", exec_time
     return float(num_knots)/len(results_file)
 
 if __name__ == '__main__':
